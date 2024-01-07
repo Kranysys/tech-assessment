95cbd6c9fd9d5369049d07974e33635e192b96f0
Nicolas <33665069+Kranysys@users.noreply.github.com>
2024-01-07 01:48:53 +0100

    feat: isEligible compute cart eligibility

--- src/eligibility.service.js
+++ src/eligibility.service.js
@@ -8,8 +8,66 @@ class EligibilityService {
    * @return {boolean}
    */
   isEligible(cart, criteria) {
-    // TODO: compute cart eligibility here.
-    return false;
+    for (const key in criteria) {
+      if (!criteria.hasOwnProperty(key)) continue;
+
+      const cartValue = this.getNestedValue(cart, key);
+      const criteriaValue = criteria[key];
+
+      if (!this.isConditionFulfilled(cartValue, criteriaValue)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  getNestedValue(obj, key) {
+    return key.split(".").reduce((value, keyPart) => {
+      if (value && typeof value === "object") {
+        return Array.isArray(value)
+          ? value.map((subObj) => subObj[keyPart])
+          : value[keyPart];
+      }
+      return undefined;
+    }, obj);
+  }
+
+  isConditionFulfilled(cartValue, criteriaValue) {
+    if (cartValue === undefined) return false;
+
+    // Handling arrays: Check if any element in the array fulfills the condition
+    if (Array.isArray(cartValue)) {
+      return cartValue.some((element) =>
+        this.isConditionFulfilled(element, criteriaValue)
+      );
+    }
+
+    if (typeof criteriaValue === "object" && criteriaValue !== null) {
+      if (criteriaValue.hasOwnProperty("gt")) {
+        return cartValue > criteriaValue.gt;
+      } else if (criteriaValue.hasOwnProperty("lt")) {
+        return cartValue < criteriaValue.lt;
+      } else if (criteriaValue.hasOwnProperty("gte")) {
+        return cartValue >= criteriaValue.gte;
+      } else if (criteriaValue.hasOwnProperty("lte")) {
+        return cartValue <= criteriaValue.lte;
+      } else if (criteriaValue.hasOwnProperty("in")) {
+        return criteriaValue.in.includes(cartValue);
+      } else if (criteriaValue.hasOwnProperty("and")) {
+        return Object.keys(criteriaValue.and).every((key) =>
+          this.isConditionFulfilled(cartValue, {
+            [key]: criteriaValue.and[key],
+          })
+        );
+      } else if (criteriaValue.hasOwnProperty("or")) {
+        return Object.keys(criteriaValue.or).some((key) =>
+          this.isConditionFulfilled(cartValue, { [key]: criteriaValue.or[key] })
+        );
+      }
+    }
+
+    return cartValue == criteriaValue;
   }
 }
 
